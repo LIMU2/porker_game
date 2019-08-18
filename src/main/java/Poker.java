8fd3fcea58d28a0cd8b75a37f5923e3b91dccdb6
murphylee2048 <1134697131@qq.com>
2019-08-18 23:35:07 +0800

    create Poker class

--- src/main/java/Poker.java
+++ src/main/java/Poker.java
@@ -1,2 +1,36 @@
 public class Poker {
+    private String number;
+    private String suit;
+
+    public Poker(String number, String suit) {
+        this.number = number;
+        this.suit = suit;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int compareToSinglePoker(Poker poker) {
+        if (Integer.parseInt(this.getNumber()) > Integer.parseInt(poker.getNumber())) {
+            return 1;  // 大于
+        } else if (Integer.parseInt(this.getNumber()) < Integer.parseInt(poker.getNumber())) {
+            return -1;  // 小于
+        } else {
+            return 0;  // draw
+        }
+    }
+
 }
