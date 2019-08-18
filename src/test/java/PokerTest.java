c968b98a1c29e6fa006a85115ef80cd3b448e566
murphylee2048 <1134697131@qq.com>
2019-08-18 23:34:07 +0800

    test: should_return_1_when_compare_single_poker_given_porkerA_4H_porkerB_3H

--- src/test/java/PokerTest.java
+++ src/test/java/PokerTest.java
@@ -1,5 +1,18 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PokerTest {
+    @Test
+    public void should_return_1_when_compare_single_poker_given_porkerA_4H_porkerB_3H() {
+        // given
+        Poker pokerA = new Poker("5", "H");
+        Poker pokerB = new Poker("3", "S");
 
+        // when
+        int res = pokerA.compareToSinglePoker(pokerB);
+
+        // then
+        assertEquals(1, res);
+    }
 }
