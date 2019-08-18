dd67512157db3e2e7df5b36f58f927ff4147ea3b
murphylee2048 <1134697131@qq.com>
2019-08-18 23:36:56 +0800

    create Player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,31 @@
+import java.util.List;
+
 public class Player {
+    private String name;
+    private List<Poker> pokers;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Poker> getPokers() {
+        return pokers;
+    }
+
+    public void setPokers(List<Poker> pokers) {
+        this.pokers = pokers;
+    }
+
+    public Player(List<Poker> pokers) {
+        this.pokers = pokers;
+    }
+
+    public Player(String name, List<Poker> pokers) {
+        this.name = name;
+        this.pokers = pokers;
+    }
 }
