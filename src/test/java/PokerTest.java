3a053f9121fbd3fc5877d4c562107d2ce379eef7
murphylee2048 <1134697131@qq.com>
2019-08-19 08:31:43 +0800

    test: should_return_playerA_lose_when_playerA_vs_playerB_given_playerA_3H_3D_5S_5C_3D_playerB_3H_3D_3S_5C_3D

--- src/test/java/PokerTest.java
+++ src/test/java/PokerTest.java
@@ -152,4 +152,19 @@ public class PokerTest {
         // then
         assertEquals("playerA Win", res);
     }
+
+    @Test
+    public void should_return_playerA_lose_when_playerA_vs_playerB_given_playerA_3H_3D_5S_5C_3D_playerB_3H_3D_3S_5C_3D() {
+        // given
+        Player playerA = new Player("playerA",
+                Arrays.asList(new Poker("3", "H"), new Poker("3", "D"), new Poker("5", "S"), new Poker("5", "C"), new Poker("3", "D")));
+        Player playerB = new Player("playerB",
+                Arrays.asList(new Poker("3", "H"), new Poker("3", "D"), new Poker("3", "S"), new Poker("5", "C"), new Poker("3", "D")));
+
+        // when
+        String res = playerA.vs(playerB);
+
+        // then
+        assertEquals("playerA Lose", res);
+    }
 }
